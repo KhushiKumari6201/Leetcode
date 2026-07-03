651e57c682051d77cc56aa9eee9e0e1a72e73122
Khushi Kumari <246981926+KhushiKumari6201@users.noreply.github.com>
2026-07-03 18:54:02 +0530

    Time: 118 ms (52.48%) | Memory: 162.7 MB (82.18%) - LeetSync

---
+++ 3919-network-recovery-pathways/network-recovery-pathways.java
@@ -0,0 +1,88 @@
+import java.util.*;
+
+class Solution {
+    public int findMaxPathScore(int[][] edges, boolean[] online, long k) {
+        int n = online.length;
+
+        List<int[]>[] graph = new ArrayList[n];
+        int[] indegree = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        int maxEdge = 0;
+
+        for (int[] e : edges) {
+            graph[e[0]].add(new int[]{e[1], e[2]});
+            indegree[e[1]]++;
+            maxEdge = Math.max(maxEdge, e[2]);
+        }
+
+        // Topological Sort
+        int[] topo = new int[n];
+        int idx = 0;
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) q.offer(i);
+        }
+
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            topo[idx++] = u;
+
+            for (int[] e : graph[u]) {
+                if (--indegree[e[0]] == 0) {
+                    q.offer(e[0]);
+                }
+            }
+        }
+
+        int low = 0;
+        int high = maxEdge;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (check(mid, graph, topo, online, k)) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    private boolean check(int limit, List<int[]>[] graph, int[] topo,
+                          boolean[] online, long k) {
+
+        int n = online.length;
+        long INF = Long.MAX_VALUE / 4;
+
+        long[] dp = new long[n];
+        Arrays.fill(dp, INF);
+        dp[0] = 0;
+
+        for (int u : topo) {
+            if (dp[u] == INF) continue;
+
+            if (u != 0 && u != n - 1 && !online[u]) continue;
+
+            for (int[] edge : graph[u]) {
+                int v = edge[0];
+                int cost = edge[1];
+
+                if (cost < limit) continue;
+                if (v != n - 1 && !online[v]) continue;
+
+                dp[v] = Math.min(dp[v], dp[u] + cost);
+            }
+        }
+
+        return dp[n - 1] <= k;
+    }
+}
